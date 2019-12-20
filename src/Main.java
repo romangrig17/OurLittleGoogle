cd1e0209766ee01b5c149d7e5ad1449d6887196e
grigorer <romangrig17@gmail.com>
2019-12-20 10:06:06 +0200

    20/12 10:05

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,13 @@ import ViewModel.Manager;
 public class Main {
 
     public static void main(String[] args) {
-        //GUI gui = new GUI();
-        System.out.println((Math.abs(("POUNDS".toLowerCase()).hashCode() % 1000)));
-        System.out.println((Math.abs(("pounds".toLowerCase()).hashCode() % 1000)));
-        System.out.println((Math.abs("POUNDS".toLowerCase().hashCode() % 1000)));
-        System.out.println((Math.abs("pounds".toLowerCase().hashCode() % 1000)));
+        GUI gui = new GUI();
 
-        Manager manager = new Manager();
-        manager.setStemming(true);
-        manager.setPathForCorpus("D:\\My Little Project\\corpus\\corpus");
-        manager.setPathForPostingFile("D:\\My Little Project\\PostingFile");
-        manager.run();
+//        Manager manager = new Manager();
+//        manager.setStemming(true);
+//        manager.setPathForCorpus("D:\\My Little Project\\corpus\\corpus");
+//        manager.setPathForPostingFile("D:\\My Little Project\\PostingFile");
+//        manager.run();
     }
 
 
