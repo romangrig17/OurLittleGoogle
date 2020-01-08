26e6b486aa598d9543f2647f374e30ef13471713
chendahan <chen1dahan1@gmail.com>
2020-01-08 17:35:12 +0200

    added doc class # Conflicts: #	src/Main.java #	src/ViewModel/Manager.java

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ import ViewModel.Manager;
 public class Main {
 
     public static void main(String[] args) {
-        GUI gui = new GUI();
-
+        //GUI gui = new GUI();
 
 //        Manager manager = new Manager();
 //        manager.setStemming(false);
@@ -20,5 +19,18 @@ public class Main {
 //        manager.searchQuery("coffee break BAPELA bapela");
 //        long elapsedTime = System.currentTimeMillis() - start;
 //        System.out.println("query time : "+ elapsedTime/1000F);
+
+        Manager manager = new Manager();
+        manager.setStemming(false);
+        manager.setPathForCorpus("C:\\corpus_MINI");
+        //manager.setPathForCorpus("D:\\corpus2");
+        manager.setPathForPostingFile("C:\\PostingFileMINI");
+        manager.run();
+        manager.loadDictionary(false);
+        
+        long start = System.currentTimeMillis();    
+        manager.searchQuery("coffee break BAPELA bapela");
+        long elapsedTime = System.currentTimeMillis() - start;
+        System.out.println("query time : "+ elapsedTime/1000F);
     }
 }
